6cd6063b9a92dbf513a554987260149d90e30585
xwolalala <3206574146@qq.com>
2018-08-05 10:39:52 +0800

    1036

--- 1036.c
+++ 1036.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main ()
 {
-    int n,hang,i,j;
+    int N,hang,i,j;
     char f;
-    scanf("%d %c",&n,&f);
-    hang=(n*10/2+5)/10;
-    for(i=0;i<n;i++)
+    scanf("%d %c",&N,&f);
+    hang=(N*10/2+5)/10;
+    for(i=0;i<N;i++)
     {
         printf("%c",f);
     }
@@ -13,15 +13,17 @@ int main ()
     for(i=0;i<hang-2;i++)
     {
         printf("%c",f);
-        for(j=0;j<n-2;j++)
+        for(j=0;j<N-2;j++)
         {
             printf(" ");
         }
         printf("%c\n",f);
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<N;i++)
     {
         printf("%c",f);
     }
     return 0;
+
+
 }
